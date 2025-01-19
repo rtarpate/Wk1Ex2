18bb2d406ee21a70e8821877afec2c2b64e28095
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:18:30 -0800

    Add project files.

---
+++ Wk1Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// get users first name
+Console.WriteLine("What is your first name?");
+
+// declares firstname variable
+string firstname = Console.ReadLine();
+
+// get users last name
+Console.WriteLine("What is your last name?");
+
+// declares lastname variable
+string lastname = Console.ReadLine();
+
+// get users age
+Console.WriteLine("What is your age?");
+
+// decalres age variable
+string age = Console.ReadLine();
+
+// get users favoirte color
+Console.WriteLine("What is your favorite color?");
+
+// decalres color variable
+string color = Console.ReadLine();
+
+// writes final line of assignemnts
+Console.WriteLine("Hello, " + firstname + lastname + " You are " + age + " years old and your favorite color is " + color + ".");
