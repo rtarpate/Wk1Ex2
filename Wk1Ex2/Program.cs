b788814172b14071bcff40e28ef07b16583e9dbb
Ashton Pate <arpate@amarillocollege.com>
2025-01-19 15:20:47 -0800

    commeted all code

--- Wk1Ex2/Program.cs
+++ Wk1Ex2/Program.cs
@@ -1,27 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// get first name
 Console.WriteLine("What is your first name?");
 
-
+// decalres first name variable
 string firstname = Console.ReadLine();
 
-
+// gets last name
 Console.WriteLine("What is your last name?");
 
-
+// declares last name varibale
 string lastname = Console.ReadLine();
 
-
+// gets age
 Console.WriteLine("What is your age?");
 
-
+// declares age varible
 string age = Console.ReadLine();
 
-
+// gets fav color
 Console.WriteLine("What is your favorite color?");
 
-
+// decalres fav color varible
 string color = Console.ReadLine();
 
-
+// writes final line
 Console.WriteLine("Hello, " + firstname + lastname + " You are " + age + " years old and your favorite color is " + color + ".");
